07b2a48f7e43f6857382a434867c0073ab90a3ac
Corentin Gay <corentin.gay060@gmail.com>
2018-04-06 12:32:53 +0200

    cmd_line: we now get the file from the command line

---
+++ gpr2ld/NOTES.md
@@ -0,0 +1,3 @@
+Notes
++ For GNATCOLL VFS should Create_From_Base fail when passed a non existent file ?
++ Rename Build() that returns an Attribute_Pkg_List?String to something else
--- gpr2ld/src/device.adb
+++ gpr2ld/src/device.adb
@@ -1,13 +1,24 @@
+with GNATCOLL.VFS; use GNATCOLL.VFS;
+with GNAT.Strings; use GNAT.Strings;
+with Ada.Text_IO; use Ada.Text_IO;
+
 package body Device is
 
-   procedure Set_Memory_List (This : in out Spec; Path : String)
+   procedure Set_Memory_List (Self : in out Spec; Path : String)
    is
       Tree : Project_Tree;
       Root : Project_Type;
-      VFS : Virtual_File := Create_From_Base (Filesystem_String (Path));
-      Memory_List : Attribute_Pkg_List := Build ("memory", "memory_list");
-      Size_Table : Attribute_Pkg_String := Build ("memory", "size");
-      Start_Table : Attribute_Pkg_String := Build ("memory", "start");
+      VFS : constant Virtual_File := Create_From_Base
+                                       (Filesystem_String (Path));
+      Memory_List : constant Attribute_Pkg_List := Build
+                                       ("memory",
+                                        "memory_list");
+      Size_Table : constant Attribute_Pkg_String := Build
+                                       ("memory",
+                                        "size");
+      Start_Table : constant Attribute_Pkg_String := Build
+                                       ("memory",
+                                        "start");
 
    begin
       GNATCOLL.Projects.Load (Tree,
@@ -18,16 +29,17 @@ package body Device is
 
       for Memory of Root.Attribute_Value (Memory_List).all loop
          declare
-            Size : String := Root.Attribute_Value (Size_Table,
+            Size : constant String := Root.Attribute_Value (Size_Table,
                                                    Index => Memory.all);
 
-            Start : String := Root.Attribute_Value (Start_Table,
+            Start : constant String := Root.Attribute_Value (Start_Table,
                                                    Index => Memory.all);
-            Memory_Unit : Memory_Type := (
-                                       Name => To_Unbounded_String ("TestRAM"),
-                                       Start => 16#0#,
-                                       Size => 16#100#,
-                                       Kind => RAM);
+           --   Memory_Unit : Memory_Type :=
+           --                         (Name => To_Unbounded_String ("TestRAM"),
+           --                          Start => 16#0#,
+           --                          Size => 16#100#,
+           --                          Kind => RAM);
+
          begin
             Put_Line ("Mem  : " & Memory.all);
             Put_Line ("Size : " & Size);
@@ -35,27 +47,31 @@ package body Device is
          end;
       end loop;
 
+      Self.CPU := (Name => To_Unbounded_String (""),
+                            Float_Handling => Soft);
    end Set_Memory_List;
 
    procedure Register_Memory_Map_Attributes
    is
-      Error1 : String := Register_New_Attribute
+      Error1 : constant String := Register_New_Attribute
                               ("Memory_List",
                               "Memory",
                               Is_List => True);
 
-      Error2 : String := Register_New_Attribute
+      Error2 : constant String := Register_New_Attribute
                            ("Size",
                            "Memory",
                            Indexed => True);
 
-      Error3 : String := Register_New_Attribute
+      Error3 : constant String := Register_New_Attribute
                            ("Start",
                            "Memory",
                            Indexed => True);
 
    begin
-      null;
+      if Error1 /= "" or else Error2 /= "" or else Error3 /= "" then
+         raise Program_Error;
+      end if;
    end Register_Memory_Map_Attributes;
 
 end Device;
