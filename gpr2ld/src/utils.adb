4b060d114e209e4538521cc32a94d5a09b4aabcd
Corentin Gay <corentin.gay060@gmail.com>
2018-04-17 16:32:56 +0200

    utils: change error handling code for registering attributes

--- gpr2ld/src/utils.adb
+++ gpr2ld/src/utils.adb
@@ -2,32 +2,39 @@ package body Utils is
 
    procedure Register_Memory_Map_Attributes
    is
-      Error1 : constant String := Register_New_Attribute
+      Error1 : aliased constant String := Register_New_Attribute
                               ("Mem_Kind",
                               "Memory",
                               Indexed => True);
 
-      Error2 : constant String := Register_New_Attribute
+      Error2 : aliased constant String := Register_New_Attribute
                            ("Size",
                            "Memory",
                            Indexed => True);
 
-      Error3 : constant String := Register_New_Attribute
+      Error3 : aliased constant String := Register_New_Attribute
                            ("Start",
                            "Memory",
                            Indexed => True);
 
-      Error4 : constant String := Register_New_Attribute
+      Error4 : aliased constant String := Register_New_Attribute
                            ("Memories",
                            "Memory",
                            Is_List => True);
 
+      type Err_Str_Access is access constant String;
+      type Strings is array (Integer range <>) of Err_Str_Access;
+      Errors : constant Strings :=
+         (1 => Error1'Access,
+          2 => Error2'Access,
+          3 => Error3'Access,
+          4 => Error4'Access);
    begin
-      if Error1 /= "" or else Error2 /= "" or else Error3 /= ""
-         or else Error4 /= ""
-      then
-         raise Program_Error with "Could not register new attribute.";
-      end if;
+      for Str of Errors loop
+         if Str.all /= "" then
+            raise Program_Error with "Could not register new attribute.";
+         end if;
+      end loop;
    end Register_Memory_Map_Attributes;
 
 end Utils;
--- gpr2ld/src/utils.ads
+++ gpr2ld/src/utils.ads
@@ -1,6 +1,12 @@
 with GNATCOLL.Projects; use GNATCOLL.Projects;
 
+with Ada.Containers.Vectors;
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+
 package Utils is
+
+   package UString_Vect is new
+           Ada.Containers.Vectors (Positive, Unbounded_String);
 
    procedure Register_Memory_Map_Attributes with
       Post => (Attribute_Registered ("Mem_Kind", "Memory")
