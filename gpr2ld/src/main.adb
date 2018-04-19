e6653bd7634d2803ecc96a68c20018867bf1a426
Corentin Gay <corentin.gay060@gmail.com>
2018-04-19 11:08:13 +0200

    projects: first iteration of Project_Scope class

--- gpr2ld/src/main.adb
+++ gpr2ld/src/main.adb
@@ -1,6 +1,7 @@
 with GNAT.Command_Line;
 with GNAT.Strings;
 
+with Ada.Strings.Unbounded;
 with Ada.Text_IO; use Ada.Text_IO;
 
 with GNATCOLL.Projects; use GNATCOLL.Projects;
@@ -9,13 +10,11 @@ with GNATCOLL.VFS; use GNATCOLL.VFS;
 with Setup;
 with Utils;
 with Device; use Device;
-
---  with Ada.Text_IO; use Ada.Text_IO;
+with Project_View; use Project_View;
 
 procedure Main is
    use GNAT.Command_Line;
    use GNAT.Strings;
-
    --  Spec representing the target device
    Spec : Device.Spec;
 
@@ -38,14 +37,52 @@ begin
       VFS : constant Virtual_File := Create_From_Base
                                      (Filesystem_String (Config_File.all));
       Tree : Project_Tree;
+
+      use Ada.Strings.Unbounded;
+      function TUS (Source : String) return Unbounded_String
+         renames To_Unbounded_String;
+
+      use Attr_Vect;
+      use Pack_Vect;
+
+      Package_CPU : constant Project_Package :=
+                 (Name => TUS ("CPU"),
+                  Attributes =>
+                     (Name => TUS ("Name"),
+                        others => <>) &
+                     (Name => TUS ("Float_Handling"),
+                        others => <>)
+               );
+
+      Package_Memory : constant Project_Package :=
+                 (Name => TUS ("Memory"),
+                  Attributes =>
+                     (Name => TUS ("Memories"),
+                           Is_List => True,
+                           others => <>) &
+
+                     (Name => TUS ("Start"),
+                           Is_Indexed => True,
+                           others => <>) &
+
+                     (Name => TUS ("Size"),
+                           Is_Indexed => True,
+                           others => <>) &
+
+                     (Name => TUS ("Mem_Kind"),
+                           Is_Indexed => True,
+                           others => <>)
+               );
+      Project_Scope : Scope;
    begin
+      Project_Scope.Make (Package_CPU & Package_Memory);
       Tree.Load (Root_Project_Path => VFS, Packages_To_Check => All_Packs);
       declare
          Project : constant Project_Type := Tree.Root_Project;
       begin
-         Spec.Get_Memory_List_From_File (Project);
+         Spec.Get_Memory_List_From_Project (Project);
 
-         Spec.Get_CPU_From_File (Project);
+         Spec.Get_CPU_From_Project (Project);
 
          Spec.Display;
       end;
