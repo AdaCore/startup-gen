8bd1999d4c18d649cc12c3a84856816367bfbfb2
Corentin Gay <corentin.gay060@gmail.com>
2018-04-23 11:41:19 +0200

    linker : add file writer to handle indentation levels

---
+++ gpr2ld/src/file_writer.adb
@@ -0,0 +1,49 @@
+with GNAT.Strings; use GNAT.Strings;
+with Ada.Text_IO;
+
+package body File_Writer is
+
+   procedure Put_Line (Self : in out Indented_File_Writer;
+                       Line : String;
+                       Indented : Boolean := False) is
+      Indentation : Unbounded_String := To_Unbounded_String ("");
+   begin
+
+      if Indented then
+         for I in Integer
+            range 1 .. Self.Indentation_Level * Self.Indentation_Size
+         loop
+            Indentation := Indentation & Self.Indentation_Character;
+         end loop;
+      end if;
+
+      GNATCOLL.VFS.Write (Self.Handle,
+         To_String (Indentation) & Line & ASCII.LF);
+   end Put_Line;
+
+   procedure Put_Line (Self : in out Indented_File_Writer;
+                    Line : Unbounded_String;
+                    Indented : Boolean := False) is
+   begin
+      Self.Put_Line (To_String (Line), Indented);
+   end Put_Line;
+
+
+   function Make (Handle : GNATCOLL.VFS.Writable_File;
+                  Indentation_Size : Integer := 2;
+                  Indentation_Character : Character := ' ')
+                  return Indented_File_Writer is
+
+   begin
+      return (Handle => Handle,
+          Indentation_Level => 0,
+          Indentation_Size => Indentation_Size,
+          Indentation_Character => Indentation_Character);
+   end Make;
+
+   procedure Close (Self : in out Indented_File_Writer) is
+   begin
+      GNATCOLL.VFS.Close (Self.Handle);
+   end Close;
+
+end File_Writer;
---
+++ gpr2ld/src/file_writer.ads
@@ -0,0 +1,45 @@
+with GNATCOLL.VFS;
+
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with Ada.Finalization;
+
+package File_Writer is
+
+   type Indented_File_Writer is tagged private;
+
+   function Make (Handle : GNATCOLL.VFS.Writable_File;
+                  Indentation_Size : Integer := 2;
+                  Indentation_Character : Character := ' ')
+                  return Indented_File_Writer;
+
+   procedure Put_Line (Self : in out Indented_File_Writer;
+                    Line : String;
+                    Indented : Boolean := False);
+
+   procedure Put_Line (Self : in out Indented_File_Writer;
+                    Line : Unbounded_String;
+                    Indented : Boolean := False);
+
+
+   procedure Put_Indented_Line (Self : in out Indented_File_Writer;
+                                Line : String;
+                                Indented : Boolean := True) renames Put_Line;
+
+   procedure Put_Indented_Line (Self : in out Indented_File_Writer;
+                                Line : Unbounded_String;
+                                Indented : Boolean := True) renames Put_Line;
+
+
+   procedure Close (Self : in out Indented_File_Writer);
+
+private
+
+   type Indented_File_Writer is tagged
+      record
+         Handle : GNATCOLL.VFS.Writable_File;
+         Indentation_Level : Integer;
+         Indentation_Size : Integer;
+         Indentation_Character : Character;
+      end record;
+
+end File_Writer;
