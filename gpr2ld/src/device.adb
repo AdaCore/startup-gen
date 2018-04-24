8a8f9e62854d4dac45df2b57c1cf98da4986168c
Corentin Gay <corentin.gay060@gmail.com>
2018-04-24 10:29:22 +0200

    gpr2ld,style: fix coding style and add comment boxes
    this fixes AdaCore/Bare_Metal_Project_Generator/#4

--- gpr2ld/src/device.adb
+++ gpr2ld/src/device.adb
@@ -1,12 +1,20 @@
 with Ada.Text_IO; use Ada.Text_IO;
 
+------------
+-- Device --
+------------
+
 package body Device is
+
+   -------------
+   -- Display --
+   -------------
 
    procedure Display (Self : in out Spec) is
    begin
       Put_Line ("CPU: " & To_String (Self.CPU.Name));
-      Put_Line ("Float_Handling: " &
-                 Float_Type'Image (Self.CPU.Float_Handling));
+      Put_Line
+        ("Float_Handling: " & Float_Type'Image (Self.CPU.Float_Handling));
       for Memory of Self.Memory loop
          Put_Line ("Name : " & To_String (Memory.Name));
          Put_Line ("Start : " & To_String (Memory.Start));
@@ -15,7 +23,10 @@ package body Device is
       end loop;
    end Display;
 
-   --  Dump Linker Script --
+   ------------------------
+   -- Dump_Linker_Script --
+   ------------------------
+
    procedure Dump_Linker_Script (Self : in out Spec; VF : Virtual_File) is
       File : Indented_File_Writer := Make (Handle => Write_File (VF));
    begin
@@ -29,8 +40,13 @@ package body Device is
       File.Close;
    end Dump_Linker_Script;
 
+   -------------------
+   -- Dump_Sections --
+   -------------------
+
    procedure Dump_Sections (Self : in out Spec;
-                            File : in out Indented_File_Writer) is
+      File                       : in out Indented_File_Writer)
+   is
    begin
       File.Put_Line ("SECTIONS");
       File.Put_Line ("{");
@@ -42,36 +58,42 @@ package body Device is
       File.Put_Line ("}");
    end Dump_Sections;
 
+   ----------------------------------
+   -- Get_Memory_List_From_Project --
+   ----------------------------------
+
    procedure Get_Memory_List_From_Project (Self : in out Spec;
-                                        Spec_Project : Project_Type) is
+      Spec_Project                              :        Project_Type)
+   is
       use Mem_Vect;
 
       Memory_List : constant Attribute_Pkg_List :=
-                     Build ("Memory_map", "memories");
+        Build ("Memory_map", "memories");
       Size_Table : constant Attribute_Pkg_String :=
-                     Build ("Memory_map", "Size");
+        Build ("Memory_map", "Size");
       Address_Table : constant Attribute_Pkg_String :=
-                     Build ("Memory_map", "Address");
+        Build ("Memory_map", "Address");
       Kind_Table : constant Attribute_Pkg_String :=
-                     Build ("Memory_map", "mem_kind");
+        Build ("Memory_map", "mem_kind");
 
    begin
       for Memory of Spec_Project.Attribute_Value (Memory_List).all loop
          declare
-            Size : constant String := Spec_Project.
-                     Attribute_Value (Size_Table, Index => Memory.all);
+            Size : constant String :=
+              Spec_Project.Attribute_Value (Size_Table, Index => Memory.all);
 
-            Start : constant String := Spec_Project.
-                     Attribute_Value (Address_Table, Index => Memory.all);
+            Start : constant String :=
+              Spec_Project.Attribute_Value
+                (Address_Table, Index => Memory.all);
 
-            Kind : constant String := Spec_Project.
-                     Attribute_Value (Kind_Table, Index => Memory.all);
+            Kind : constant String :=
+              Spec_Project.Attribute_Value (Kind_Table, Index => Memory.all);
 
             Memory_Unit : constant Memory_Type :=
-                                  (Name => To_Unbounded_String (Memory.all),
-                                   Start => To_Unbounded_String (Start),
-                                   Size => To_Unbounded_String (Size),
-                                   Kind => Memory_Kind'Value (Kind));
+              (Name  => To_Unbounded_String (Memory.all),
+               Start => To_Unbounded_String (Start),
+               Size  => To_Unbounded_String (Size),
+               Kind  => Memory_Kind'Value (Kind));
          begin
             Self.Memory := Self.Memory & Memory_Unit;
          end;
@@ -79,17 +101,22 @@ package body Device is
 
    end Get_Memory_List_From_Project;
 
+   --------------------------
+   -- Get_CPU_From_Project --
+   --------------------------
+
    procedure Get_CPU_From_Project (Self : in out Spec;
-                                Spec_Project : Project_Type) is
+      Spec_Project                      :        Project_Type)
+   is
 
-      Name : constant String :=  Spec_Project.
-                           Attribute_Value (Build ("cpu", "name"));
+      Name : constant String :=
+        Spec_Project.Attribute_Value (Build ("cpu", "name"));
 
-      Float_Handling : constant String :=  Spec_Project.
-                           Attribute_Value (Build ("cpu", "float_handling"));
+      Float_Handling : constant String :=
+        Spec_Project.Attribute_Value (Build ("cpu", "float_handling"));
    begin
-      Self.CPU := (To_Unbounded_String (Name),
-                   Float_Type'Value (Float_Handling));
+      Self.CPU :=
+        (To_Unbounded_String (Name), Float_Type'Value (Float_Handling));
 
    end Get_CPU_From_Project;
 
--- gpr2ld/src/utils.ads
+++ gpr2ld/src/utils.ads
@@ -3,10 +3,18 @@ with GNATCOLL.Projects; use GNATCOLL.Projects;
 with Ada.Containers.Vectors;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
+-----------
+-- Utils --
+-----------
+
 package Utils is
 
-   package UString_Vect is new
-           Ada.Containers.Vectors (Positive, Unbounded_String);
+   package UString_Vect is new Ada.Containers.Vectors (Positive,
+      Unbounded_String);
+
+   ------------------------------------
+   -- Register_Memory_Map_Attributes --
+   ------------------------------------
 
    procedure Register_Memory_Map_Attributes;
 
