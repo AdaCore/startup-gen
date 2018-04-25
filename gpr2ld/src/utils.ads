85880a9d104a64bb591666916ab78c6766171738
Corentin Gay <corentin.gay060@gmail.com>
2018-04-25 17:56:32 +0200

    gpr2ld,style: remove unused package

--- gpr2ld/src/utils.ads
+++ gpr2ld/src/utils.ads
@@ -1,16 +1,10 @@
 with GNATCOLL.Projects; use GNATCOLL.Projects;
 
-with Ada.Containers.Vectors;
-with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
-
 -----------
 -- Utils --
 -----------
 
 package Utils is
-
-   package UString_Vect is new Ada.Containers.Vectors (Positive,
-      Unbounded_String);
 
   procedure Register_Memory_Map_Attributes;
 
