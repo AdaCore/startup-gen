e27f535cd6b2e7d04ebebc0782bf376795dcc5e3
Corentin Gay <corentin.gay060@gmail.com>
2018-04-26 10:04:52 +0200

    gpr2ld,style: fix alignement

--- gpr2ld/src/sections.ads
+++ gpr2ld/src/sections.ads
@@ -7,25 +7,24 @@ with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
 package Sections is
 
-  package Unbounded_String_Vect is new Ada.Containers.Vectors
+   package Unbounded_String_Vectors is new Ada.Containers.Vectors
       (Positive, Unbounded_String);
 
    type Section is tagged record
-      Name : Unbounded_String;
-      Reloc_Memory : Unbounded_String;
-      To_Init : Boolean;
-      Additional_Content : Unbounded_String_Vect.Vector;
+      Name               : Unbounded_String;
+      Reloc_Memory       : Unbounded_String;
+      To_Init            : Boolean;
+      Additional_Content : Unbounded_String_Vectors.Vector;
    end record;
 
    function Make_Section
-      (Boot_Memory : Unbounded_String;
-       Name : Unbounded_String;
+      (Boot_Memory  : Unbounded_String;
+       Name         : Unbounded_String;
        Reloc_Memory : Unbounded_String;
-       Force_Init : Boolean := False)
+       Force_Init   : Boolean := False)
        return Section;
 
-package Sect_Vect is new Ada.Containers.Vectors
-      (Element_Type => Sections.Section,
-       Index_Type   => Positive);
+   package Section_Vectors is new Ada.Containers.Vectors
+      (Positive, Sections.Section);
 
 end Sections;
