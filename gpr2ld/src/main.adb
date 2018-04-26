8fbc2baddf8a76d6f766ed38f16cb0f2eb6d4e73
Corentin Gay <corentin.gay060@gmail.com>
2018-04-26 18:18:29 +0200

    gpr2ld,style: fix alignement

--- gpr2ld/src/main.adb
+++ gpr2ld/src/main.adb
@@ -39,12 +39,12 @@ begin
 
       Linker_Script : constant Virtual_File :=
         Create_From_Dir
-         (Dir => Create (Filesystem_String (Input.Output_Dir.all)),
+         (Dir       => Create (Filesystem_String (Input.Output_Dir.all)),
           Base_Name => Filesystem_String (Input.Linker_File.all));
 
       Memory_Map : constant Virtual_File :=
         Create_From_Dir
-         (Dir => Create (Filesystem_String (Input.Output_Dir.all)),
+         (Dir       => Create (Filesystem_String (Input.Output_Dir.all)),
           Base_Name => Filesystem_String (Input.Memory_Map_File.all));
 
    begin
