9a46318f43fb24596e3b7edfb14d255f241a5db0
Corentin Gay <corentin.gay060@gmail.com>
2018-05-25 17:25:08 +0200

    knowledgedbtools: add tests for profiling the python modules

--- knowledge_db_generator/test_query.py
+++ knowledge_db_generator/test_query.py
@@ -1,18 +1,39 @@
 import querybdb
 import modifybdb
 import json
+import cProfile
+import re
+import os
+import glob
 
-db = "bdb"
 
-#for row in querybdb.query_names("tree", db):
-#    print row
 
-device_name = "Z32F38412ALS"
+def modify_test():
+    db = "bdb"
+    for f in glob.glob("cmsis_packs/*.pack"):
+        modifybdb.add_package(os.path.abspath(f), db)
 
-info = querybdb.query_device(device_name, db)
+def query_test():
+    db = "bdb"
+    packages = querybdb.query_names("package", db)
+    for package in packages:
+        print package
 
-out = querybdb.get_json_output_for_device(device_name, db)
-json_output = json.dumps(out)
+#for board_name in querybdb.query_names("board", db):
+#    print board_name
 
-print json_output
+#device_name = "Z32F38412ALS"
+#
+#devices = querybdb.query_devices(db)
+#
+#out = querybdb.get_json_output_for_device(device_name, db)
+#json_output = json.dumps(out)
+#
+#print json_output
 #project_file, interrupts = json2gpr.translate(json_output)
+
+#modify_test()
+
+cProfile.run('modify_test()')
+      
+
