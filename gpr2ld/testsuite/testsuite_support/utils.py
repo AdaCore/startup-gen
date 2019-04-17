302bf19f98fd8eab6e2ea49a36eda9fa35840cff
Fabien Chouteau <chouteau@adacore.com>
2019-04-17 15:44:27 +0200

    Add a test for the STM32F407 layout (Cortex-M4F, armv7e-m)
    This test covers generation of crt0 and linker script, compilation and
    execution of the compiled code. It also "boots" from each of the three
    different memory areas.

--- gpr2ld/testsuite/testsuite_support/utils.py
+++ gpr2ld/testsuite/testsuite_support/utils.py
@@ -8,10 +8,27 @@ def contents_of(filename):
     return contents
 
 def run_tool(args, output='gpr2ld.out', error='gpr2ld.err'):
-    p = Run(['gpr2ld'] + args,
-            output=output, error=error)
+    p = Run(['gpr2ld'] + args, output=output, error=error)
 
     if p.status != 0:
         print "command failed:"
+        print contents_of(output)
+        print contents_of(error)
+
+
+def gprbuild(args, output='gprbuild.out', error='gprbuild.err'):
+    p = Run(['gprbuild'] + args, output=output, error=error)
+
+    if p.status != 0:
+        print "Build failed:"
+        print contents_of(output)
+        print contents_of(error)
+
+
+def gnatemu(args, output='gnatemu.out', error='gnatemu.err'):
+    p = Run(['arm-eabi-gnatemu'] + args, output=output, error=error)
+
+    if p.status != 0:
+        print "GNATemu failed:"
         print contents_of(output)
         print contents_of(error)
