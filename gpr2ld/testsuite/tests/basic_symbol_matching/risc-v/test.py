15e8e272c114f584a86db98b478b49aa4a4ffb53
Fabien Chouteau <chouteau@adacore.com>
2019-04-25 17:46:28 +0200

    Add RISC-V support and tests

---
+++ gpr2ld/testsuite/tests/basic_symbol_matching/risc-v/test.py
@@ -0,0 +1,21 @@
+from testsuite_support.utils import make_simple_project, nm_symbols, check_symbols, MemoryMap
+
+memmap = MemoryMap()
+memmap.add('flash', 'ROM', 0x20400000, 512 * 1024 * 1024)
+memmap.add('ram', 'RAM', 0x80000000, 16 * 1024)
+
+for boot_mem in memmap:
+    print "With boot_mem = %s:" % boot_mem
+    bin = make_simple_project('test_simple_project+' + boot_mem,
+                              'zfp-rv32imc',
+                              'riscv32-elf',
+                              'RISC-V32',
+                              memmap,
+                              boot_mem)
+    syms = nm_symbols (bin)
+    check_symbols (syms, memmap, [('_start', boot_mem),
+                                  ('__global_pointer$', 'ram'),
+                                  ('__stack_start', 'ram'),
+                                  ('__stack_end', 'ram'),
+                                  ('__heap_start', 'ram'),
+                                  ('__heap_end', 'ram')])
---
+++ gpr2ld/testsuite/tests/boards/hifive1/src/text_io.ads
@@ -0,0 +1,3 @@
+package Text_IO is
+   procedure Put_Line (Str : String);
+end Text_IO;
