f75ab94a231fbe78be8ec5e5c9249db2a5ced9f8
Fabien Chouteau <chouteau@adacore.com>
2019-04-25 17:46:28 +0200

    Add RISC-V support and tests

---
+++ gpr2ld/testsuite/tests/boards/hifive1/test.py
@@ -0,0 +1,13 @@
+from testsuite_support.utils import run_tool, gprbuild, runcross, contents_of
+
+
+for boot_mem in ['flash', 'ram']:
+
+    run_tool (['-P', 'prj.gpr', '-XBOOT_MEM=%s' % boot_mem,
+               '-s', 'src/crt0.S', '-l', 'src/linker.ld'])
+
+    gprbuild(['-f', '-P', 'prj.gpr', '-XBOOT_MEM=%s' % boot_mem])
+
+    runcross('riscv32-elf', 'qemu-hifive1', 'obj/main', output='runcross.out')
+
+    print contents_of('runcross.out')
