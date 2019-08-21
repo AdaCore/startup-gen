cd9e11e347804839091f948216a4f5ac4468b470
Fabien Chouteau <chouteau@adacore.com>
2019-08-21 18:01:57 +0200

    Add license and copyright notice in startup-gen

--- startup-gen/src/utils.adb
+++ startup-gen/src/utils.adb
@@ -1,3 +1,21 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                               startup-gen                                --
+--                                                                          --
+--                        Copyright (C) 2019, AdaCore                       --
+--                                                                          --
+-- This is  free  software;  you can redistribute it and/or modify it under --
+-- terms of the  GNU  General Public License as published by the Free Soft- --
+-- ware  Foundation;  either version 3,  or (at your option) any later ver- --
+-- sion.  This software is distributed in the hope  that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License for more details.  You should have received  a copy of the  GNU  --
+-- General Public License distributed with GNAT; see file  COPYING. If not, --
+-- see <http://www.gnu.org/licenses/>.                                      --
+--                                                                          --
+------------------------------------------------------------------------------
+
 with Ada.Text_IO;       use Ada.Text_IO;
 with Ada.Command_Line;  use Ada.Command_Line;
 with GNATCOLL.Projects; use GNATCOLL.Projects;
