9d9d558151674f2c6c277fc6291373574a695029
Fabien Chouteau <chouteau@adacore.com>
2020-09-11 12:55:26 +0200

    testsuite/risc-v32: test more CPU variants
    Part of T910-038.

--- testsuite/tests/basic_symbol_matching/risc-v/test.py
+++ testsuite/tests/basic_symbol_matching/risc-v/test.py
@@ -4,18 +4,25 @@ memmap = MemoryMap()
 memmap.add('flash', 'ROM', 0x20400000, 512 * 1024 * 1024)
 memmap.add('ram', 'RAM', 0x80000000, 16 * 1024)
 
-for boot_mem in memmap:
-    print "With boot_mem = %s:" % boot_mem
-    bin = make_simple_project('test_simple_project+' + boot_mem,
-                              'zfp-rv32imac',
-                              'riscv32-elf',
-                              'RISC-V32',
-                              memmap,
-                              boot_mem)
-    syms = nm_symbols (bin)
-    check_symbols (syms, memmap, [('_start', boot_mem),
-                                  ('__global_pointer$', 'ram'),
-                                  ('__stack_start', 'ram'),
-                                  ('__stack_end', 'ram'),
-                                  ('__heap_start', 'ram'),
-                                  ('__heap_end', 'ram')])
+for runtime in ['zfp-rv32i',
+                'zfp-rv32iac',
+                'zfp-rv32im',
+                'zfp-rv32imac',
+                'zfp-rv32imafc',
+                'zfp-rv32imafdc']:
+    print "Testing run-time: %s" % runtime
+    for boot_mem in memmap:
+        print "With boot_mem = %s:" % boot_mem
+        bin = make_simple_project('test_simple_project+' + boot_mem,
+                                  runtime,
+                                  'riscv32-elf',
+                                  'RISC-V32',
+                                  memmap,
+                                  boot_mem)
+        syms = nm_symbols (bin)
+        check_symbols (syms, memmap, [('_start', boot_mem),
+                                      ('__global_pointer$', 'ram'),
+                                      ('__stack_start', 'ram'),
+                                      ('__stack_end', 'ram'),
+                                      ('__heap_start', 'ram'),
+                                      ('__heap_end', 'ram')])
