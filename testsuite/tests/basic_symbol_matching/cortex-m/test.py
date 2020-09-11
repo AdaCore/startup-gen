acd9ad71dca0d727ba3cf3e977b75b9c85271569
Fabien Chouteau <chouteau@adacore.com>
2020-09-11 11:46:28 +0200

    testsuite/cortex-m: test more CPU variants
    Part of T910-038.

--- testsuite/tests/basic_symbol_matching/cortex-m/test.py
+++ testsuite/tests/basic_symbol_matching/cortex-m/test.py
@@ -4,11 +4,16 @@ memmap = MemoryMap()
 memmap.add('flash', 'ROM', 0, 32 * 1024)
 memmap.add('ram', 'RAM', 0x10000, 32 * 1024)
 
-for CPU, runtime in [('Cortex-M4F', 'zfp-cortex-m4f')]:
+for CPU, runtime in [('Cortex-M0', 'zfp-cortex-m0'),
+                     ('Cortex-M0+', 'zfp-cortex-m0p'),
+                     ('Cortex-M1', 'zfp-cortex-m1'),
+                     ('Cortex-M3', 'zfp-cortex-m3'),
+                     ('Cortex-M4', 'zfp-cortex-m4'),
+                     ('Cortex-M4F', 'zfp-cortex-m4f')]:
     print "Testing CPU: %s run-time: %s" % (CPU, runtime)
     for boot_mem in memmap:
         print "With boot_mem = %s:" % boot_mem
-        bin = make_simple_project('test_simple_project+' + boot_mem,
+        bin = make_simple_project('test_simple_project_%s_%s' % (boot_mem, runtime),
                                   runtime,
                                   'arm-eabi',
                                   CPU,
